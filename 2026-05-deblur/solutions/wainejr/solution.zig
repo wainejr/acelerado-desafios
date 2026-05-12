1420633075a17be38e301784e05fe06f51c85dd3
Waine Oliveira Jr <waine@aerosim.io>
2026-05-12 00:29:32 -0300

    added median fitler

--- 2026-05-deblur/solutions/wainejr/solution.zig
+++ 2026-05-deblur/solutions/wainejr/solution.zig
@@ -10,7 +10,23 @@ var BMP_HEADER: [BMP_HEADER_SIZE]u8 = undefined;
 
 const IMG_SIZE = SIZE_X * SIZE_Y * BMP_PIXEL_BYTES;
 var IMG_BUFFER: [SIZE_X * SIZE_Y * BMP_PIXEL_BYTES]u8 = undefined;
+var IMG_SCRATCH_BUFFER: [SIZE_X * SIZE_Y]u8 = undefined;
 const IMG_CHUNK_SIZE = 512;
+
+inline fn idx2pos(idx: usize) struct { x: i16, y: i16 } {
+    return .{ .x = @as(i16, @intCast(idx % SIZE_X)), .y = @as(i16, @intCast(idx / SIZE_X)) };
+}
+
+const IdxError = error{};
+
+inline fn pos2idx(pos: struct { x: i16, y: i16 }) IdxError!usize {
+    if ((pos.x < 0) || (pos.y < 0)) {
+        return IdxError;
+    }
+    var idx: usize = @intCast(pos.x);
+    idx += @as(usize, @intCast(pos.y)) * SIZE_X;
+    return idx;
+}
 
 fn io_img(func: anytype, fd: std.posix.fd_t) !void {
     _ = try func(fd, &BMP_HEADER);
@@ -37,7 +53,39 @@ fn img_sigma_estimator() !void {}
 fn img_deblur_filter() !void {}
 
 /// reduce white noise in image
-fn img_noise_reducer() !void {}
+fn img_noise_reducer() !void {
+    for (0..IMG_SIZE / 3) |idx| {
+        const pos = idx2pos(idx);
+
+        var n_samples: usize = 0;
+        var samples: [9]u8 = undefined;
+        for (0..3) |iy| {
+            for (0..3) |ix| {
+                const pi = .{ .x = pos.x - 1 - @as(i16, @intCast(ix)), .y = pos.y - 1 - @as(i16, @intCast(iy)) };
+
+                const pidx = pos2idx(pi) catch {
+                    break;
+                };
+                const v = IMG_BUFFER[pidx * BMP_PIXEL_BYTES];
+
+                var idx_insert: usize = 0;
+                for (0..n_samples) |iv| {
+                    if (samples[iv] > iv) {
+                        idx_insert = iv;
+                        break;
+                    }
+                }
+
+                if (idx_insert < n_samples)
+                    std.mem.copyBackwards(u8, samples[idx_insert + 1 .. n_samples + 1], samples[idx_insert..n_samples]);
+                samples[idx_insert] = v;
+                n_samples += 1;
+            }
+        }
+        const median = samples[n_samples / 2];
+        IMG_SCRATCH_BUFFER[idx] = median;
+    }
+}
 
 pub fn main() !void {
     try io_img(std.posix.read, std.posix.STDIN_FILENO);
